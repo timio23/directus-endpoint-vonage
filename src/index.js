bde448ba50e051ae49b36a5f2ef114fd10b296ce
Debian <worldyouth@Debian.vwjawvpcnaburoahavunpjgqmh.px.internal.cloudapp.net>
2023-07-28 14:14:26 +0000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+export default {
+	id: 'vonage',
+	handler: (router, { env }) => {
+		// Router config goes here
+		const axios = require('axios');
+		const vonage_host = "https://api.nexmo.com";
+		const vonage_api_key = env.VONAGE_API_KEY;
+		const vonage_secret = env.VONAGE_API_SECRET;
+		const vonage_api = axios.create({
+		 	baseURL: vonage_host,
+			auth: {
+				username: vonage_api_key,
+				password: vonage_secret,
+			},
+		});
+
+		router.get('/*', (req, res) => {
+			vonage_api.get(req.url).then((response) => {
+				res.json(response.data);
+			}).catch((error) => {
+				res.send(error);
+			});
+		});
+
+		router.post('/*', (req, res) => {
+			vonage_api.post(req.url, new URLSearchParams(req.body)).then((response) => {
+				res.json(response.data);
+			}).catch((error) => {
+				res.send(error);
+			});
+		});
+	},
+};
